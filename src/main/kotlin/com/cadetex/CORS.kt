730c6e9358841b418e432abaccb17633bdb1a4be
kevin <kevinvinograd@gmail.com>
2025-10-31 14:34:15 -0300

    Add kdtgo.com domain to CORS configuration

--- src/main/kotlin/com/cadetex/CORS.kt
+++ src/main/kotlin/com/cadetex/CORS.kt
@@ -16,8 +16,10 @@ fun Application.configureCORS() {
         // Dominios permitidos (prod)
         allowHost("kdt-frontend-prod-sa-east-1.s3-website.sa-east-1.amazonaws.com", schemes = listOf("http"))
         allowHost("don0yfk21axa5.cloudfront.net", schemes = listOf("https", "http"))
+        allowHost("kdtgo.com", schemes = listOf("https")) // Dominio personalizado con HTTPS
         // Para desarrollo local
         allowHost("localhost:5173", schemes = listOf("http"))
+        allowHost("localhost:8080", schemes = listOf("http"))
     }
 }
 
